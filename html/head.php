1b08ba84f5473fe50c07d09454f1a78816728209
JaimeBotet <jaime.90.botet@gmail.com>
2020-07-21 09:26:34 +0200

    Add head

--- html/head.php
+++ html/head.php
@@ -4,7 +4,7 @@ error_reporting(0);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>PHP Form</title>
+    <title>Assemblerfy</title>
     <!--NPM Packages -->
     <script src="node_modules/jquery/dist/jquery.js"></script>
     <script src="node_modules/bootstrap/dist/js/bootstrap.js"></script>
