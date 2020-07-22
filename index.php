898878c29f7a23f516cbba663b42ff7e70269976
guillelp1996 <40063019+guillelp1996@users.noreply.github.com>
2020-07-22 02:29:49 +0200

    fix navbar v1

--- index.php
+++ index.php
@@ -10,40 +10,54 @@ require "./html/head.php";
     <!-- Here comes a nav bar  -->
     <!-- The nav bar will contain the logo, Register Button, Log in button and Admin button  -->
     <!-- Then a search bar is displayed below -->
-        <nav class="navbar navbar-expand-lg navbar-light bg-dark rounded-sm">
-            <div class="d-flex justify-content-between w-100">
-                <div class="col-4">
+        <nav class="navbar  navbar-light bg-dark w-100 rounded-sm d-flex justify-content-center ">
+            
+                <!-- logo -->
+                <div class="col-2">
                     <img class="assembler_logo mb-3" src="https://media-exp1.licdn.com/dms/image/C560BAQHR2BMbsLm7zQ/company-logo_200_200/0?e=2159024400&v=beta&t=6CkD1A_wUzSd4CM7zn6mjo9AybK5aReonvFkuOI0WMo">
                     <a class="navbar-brand text-light page_title" href="#">Assemblerfy</a>
                 </div>
-                <div class="col-4">
-                    
-                        <input class="form-control mr-sm-2 w-100" type="search" id="searchbar" name="search" placeholder="Search" aria-label="Search">
-                        <div class = "d-flex">
-                            <select class="form-control form-control-sm w-20 mr-3 mt-2" id="type">
+                <!-- input side -->
+                <div class="row my-auto col-7">
+
+                    <div class="col-6 h-100 ">
+                        <div class="form-group">
+                            <input class="form-control w-100 h-100" type="search" id="searchbar" name="search" placeholder="Search" aria-label="Search">
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="form-group">
+                            <select class="form-control h-100" id="type">
                                 <option id="song">Song</option>
                                 <option id="album">Album</option>
                                 <option id="musicVideo">Music Video</option>
                             </select>
-                            <select class="form-control form-control-sm w-20 mr-3 mt-2" id="limit">
+                        </div>
+                    </div>
+                    <div class="col-3">
+                        <div class="form-group">
+                            <select class="form-control h-100" id="limit">
                                 <option value="25">25 results</option>
                                 <option value="50">50 results</option>
                                 <option value="100">100 results</option>
                             </select>
                         </div>
-                    
+                    </div>
+
                 </div>
-                <div class="col-4">
-                    <ul class="navbar-nav my-2 d-flex justify-content-around">
-                        <li class="nav-item w-25">
-                            <button class="btn btn-primary my-auto w-100">Login</button>
+                <!-- btn aside -->
+                    
+                <div class="col-3">
+                    <ul class="navbar form-inline d-flex justify-content-around">
+                        <li class=" col-5 ">
+                            <button class="btn btn-primary w-100">Login</button>
                         </li>
-                        <li class="nav-item w-25">
-                            <button class="btn btn-primary w-100">Register</button>
+                        <li class=" col-5">
+                            <button class="btn btn-primary w-100 ">Register</button>
                         </li>
                     </ul>
                 </div>
-            </div>
+        
         </nav>
         <!-- Then a main container where we are showing random videos  -->
         <!-- This container will contain the result of the search -> without refreshing the page?  -->
