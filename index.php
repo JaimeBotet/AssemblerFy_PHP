24f2b79417cdd9a9bbc5059e751baf1e6a326f29
guillermo-valdez <guillermovaldez8@gmail.coim>
2020-07-22 06:27:06 +0200

    fixed some bugs

--- index.php
+++ index.php
@@ -33,7 +33,7 @@ require "./html/head.php";
                 <div class="col-6 ">
                     <div class="form-group mb-0">
                         <input class="form-control w-100 h-100" type="search" id="searchbar" name="search"
-                            placeholder="Search" aria-label="Search">
+                            placeholder="Search" aria-label="Search" autofocus>
                     </div>
                 </div>
                 <div class="col">
@@ -45,7 +45,7 @@ require "./html/head.php";
                         </select>
                     </div>
                 </div>
-                
+
 
             </div>
             <!-- btn aside -->
@@ -64,12 +64,89 @@ require "./html/head.php";
         </nav>
         <!-- Then a main container where we are showing random videos  -->
         <!-- This container will contain the result of the search -> without refreshing the page?  -->
-        <div class="loading"></div>
+        <div class="loading">
+            <svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"
+                style="margin: auto; background: rgba(255, 255, 255, 0); display: block;" width="200px" height="200px"
+                viewBox="0 0 100 100" preserveAspectRatio="xMidYMid">
+                <g transform="rotate(0 50 50)">
+                    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fbfbfb">
+                        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s"
+                            begin="-0.9166666666666666s" repeatCount="indefinite"></animate>
+                    </rect>
+                </g>
+                <g transform="rotate(30 50 50)">
+                    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fbfbfb">
+                        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s"
+                            begin="-0.8333333333333334s" repeatCount="indefinite"></animate>
+                    </rect>
+                </g>
+                <g transform="rotate(60 50 50)">
+                    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fbfbfb">
+                        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.75s"
+                            repeatCount="indefinite"></animate>
+                    </rect>
+                </g>
+                <g transform="rotate(90 50 50)">
+                    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fbfbfb">
+                        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s"
+                            begin="-0.6666666666666666s" repeatCount="indefinite"></animate>
+                    </rect>
+                </g>
+                <g transform="rotate(120 50 50)">
+                    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fbfbfb">
+                        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s"
+                            begin="-0.5833333333333334s" repeatCount="indefinite"></animate>
+                    </rect>
+                </g>
+                <g transform="rotate(150 50 50)">
+                    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fbfbfb">
+                        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.5s"
+                            repeatCount="indefinite"></animate>
+                    </rect>
+                </g>
+                <g transform="rotate(180 50 50)">
+                    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fbfbfb">
+                        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s"
+                            begin="-0.4166666666666667s" repeatCount="indefinite"></animate>
+                    </rect>
+                </g>
+                <g transform="rotate(210 50 50)">
+                    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fbfbfb">
+                        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s"
+                            begin="-0.3333333333333333s" repeatCount="indefinite"></animate>
+                    </rect>
+                </g>
+                <g transform="rotate(240 50 50)">
+                    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fbfbfb">
+                        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="-0.25s"
+                            repeatCount="indefinite"></animate>
+                    </rect>
+                </g>
+                <g transform="rotate(270 50 50)">
+                    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fbfbfb">
+                        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s"
+                            begin="-0.16666666666666666s" repeatCount="indefinite"></animate>
+                    </rect>
+                </g>
+                <g transform="rotate(300 50 50)">
+                    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fbfbfb">
+                        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s"
+                            begin="-0.08333333333333333s" repeatCount="indefinite"></animate>
+                    </rect>
+                </g>
+                <g transform="rotate(330 50 50)">
+                    <rect x="47" y="24" rx="3" ry="6" width="6" height="12" fill="#fbfbfb">
+                        <animate attributeName="opacity" values="1;0" keyTimes="0;1" dur="1s" begin="0s"
+                            repeatCount="indefinite"></animate>
+                    </rect>
+                </g>
+            </svg>
+        </div>
         <div id="card_container" class="results_container mt-2 rounded-sm d-flex p-3 flex-wrap justify-content-around">
 
 
             <!-- demo card -->
-            
+
 
         </div>
 
--- src/app.js
+++ src/app.js
@@ -51,7 +51,7 @@ $(document).ready(function() {
                       <p class="card-text py-4">${result.trackName}</p>
                       <div class="card-text py-2">
                         <audio width="100%" height="auto" controls>
-                          <source src="${result.previewUrl}" type="audio/mpeg">
+                          <source src="${result.previewUrl}" >
                           no disponible
                         </audio>
                       </div>
@@ -63,13 +63,14 @@ $(document).ready(function() {
             }
           } else {
             for (let result of results) {
+              let i = 0;
               $("#card_container").append(
                 `
                 <div class="card col-3 col-md-4 col-sm-6 text-center my-5">
-                  <video class=" video-test" preload="none" poster="${result.artworkUrl100.replace("100x100", "200x200")}">
+                  <video class="video-test" id="video-${i}" preload="none" poster="${result.artworkUrl100.replace("100x100", "200x200")}">
                     <source src="${result.previewUrl}">
                   </video>
-                  <div class="btn btn-light w-25 my-2 mx-auto">play</div>
+                  <input type="button" class="btn btn-light w-25 my-2 mx-auto play" value="play">
                   <div class="card-body w-100">
                       <h5 class="card-title">${result.artistName}</h5>
                       <p class="card-text py-4">${result.trackName}</p>
@@ -77,6 +78,7 @@ $(document).ready(function() {
                 </div>
                 `
               )
+              i++
             }
           }
         }
@@ -102,4 +104,18 @@ $(document).ready(function() {
   $(document).ajaxComplete((e) => {
     $(".loading").hide()
   })
+  
+  let video = $("#video-0");
+  
+  function playPause() { 
+    if (video.paused()) 
+      video.play(); 
+    else 
+      video.pause(); 
+  } 
+  $(".play").on("click", ()=>{
+  
+    playPause()
+  })
 });
+
